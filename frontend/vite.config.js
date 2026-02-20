71751f9c82399fa7168485ff2884ee196402a4c5
Ante <golemante70@gmail.com>
2026-02-20 20:17:36 +0100

    prva faza (popravci)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { fileURLToPath } from "url"; // Dodaj ovo
 import path from "path";
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url); // Dodaj ovo
-const __dirname = path.dirname(__filename); // Dodaj ovo
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
