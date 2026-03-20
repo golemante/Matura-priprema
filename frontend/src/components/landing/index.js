83b3a959f69fc5afe0d5ff8b6c87f11be5f9dcb9
Ante <golemante70@gmail.com>
2026-03-20 17:28:46 +0100

    e sad oće

---
+++ frontend/src/components/landing/index.js
@@ -0,0 +1,8 @@
+export { HeroSection } from "./HeroSection";
+export { FloatingExamCard } from "./FloatingExamCard";
+export { StatsSection } from "./StatsSection";
+export { HowItWorksSection } from "./HowItWorksSection";
+export { PopularSubjectsSection, OtherSubjectsSection } from "./SubjectGrid";
+export { FeatureHighlightsSection } from "./FeatureHighlightsSection";
+export { CTASection } from "./CTASection";
+export { SectionHeading } from "./SectionHeading";
